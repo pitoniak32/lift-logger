0e218cd5f8cce8ef6c23bc07a6b4bd7deedfd34a
David Pitoniak <davidpitoniak32@gmail.com>
2022-05-10 20:05:20 -0400

    test: fixing e2e test

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,28 +1,28 @@
-import { Test, TestingModule } from '@nestjs/testing'
 import { INestApplication } from '@nestjs/common'
-import * as request from 'supertest'
+import { Test, TestingModule } from '@nestjs/testing'
+import { LiftLogController } from '../src/api/controllers/lift-log/lift-log.controller'
+import { LiftRootController } from '../src/api/controllers/lift-root/lift-root.controller'
+import { UserController } from '../src/api/controllers/user/user.controller'
 import { AppModule } from './../src/app.module'
 
 describe('AppController (e2e)', () => {
   let app: INestApplication
 
   beforeEach(async () => {
-    const moduleFixture: TestingModule = await Test.createTestingModule({
+    const module: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile()
 
-    app = moduleFixture.createNestApplication()
+    app = module.createNestApplication()
+
     await app.init()
   })
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!')
-  })
+  afterAll(async () => { await app.close() })
 
-  it('/:id (GET) getOneLiftLog', () => {
-    return request(app.getHttpServer()).get('/not-valid-id').expect(400)
+  it('should be defined', () => {
+    expect(app.get<LiftLogController>(LiftLogController)).toBeDefined()
+    expect(app.get<LiftRootController>(LiftRootController)).toBeDefined()
+    expect(app.get<UserController>(UserController)).toBeDefined()
   })
 })
